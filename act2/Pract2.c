45675a46e45f53fc359771c62d3c46a7c47cca0b
MBouzasG <131443918+MBouzasG@users.noreply.github.com>
2024-02-07 20:39:39 +0100

    Update Pract2.c

--- act2/Pract2.c
+++ act2/Pract2.c
@@ -4,115 +4,109 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-int main(int argc, char *argv[])
-{
-
+int main(int argc, char *argv[]) {
     // Verifica si el número de parámetros es correcto
-    if (argc != 2)
-    {
-        fprintf(stderr, "Esquema: %s <número>\n", argv[0]);
-        fprintf(stderr, "Por favor, ingrese un número entero como argumento\n");
-        return 1; // Devuelve un código de error
+    if (argc != 2) {
+        fprintf(stderr, "Error. Ingresa un unico parametro.\n");
+        return 1;
     }
 
-    // Analiza el número de entrada
-    int num_entrada = atoi(argv[1]);
+    char *endptr;
+    int num = strtol(argv[1], &endptr, 10); // Convertir a entero
 
-    // Verifica si el número de entrada está dentro del rango [0, 100]
-    if (num_entrada < 0 || num_entrada > 100)
-    {
+    // Verificar si el número es un entero
+    if (*endptr != '\0' || endptr == argv[1]) {
+        printf("No es entero.\n");
+        return 1;
+    } 
+
+    int num_terminal = atoi(argv[1]);
+
+    // Verifica si el número está dentro del rango [0, 100]
+    if (num_terminal < 0 || num_terminal > 100) {
         fprintf(stderr, "Error: El número debe estar entre 0 y 100.\n");
         return 1; 
     }
 
-    // Convertir el número de entrada a uint8_t
-    uint8_t numero = (uint8_t)num_entrada;
+    // Convertir el número pasado a uint8_t
+    uint8_t numero = (uint8_t)num_terminal;
 
-    // Pipes
+    // Define Pipes
     int p1[2], p2[2];
+    #define PadreAHijo p1
+    #define HijoAPadre p2
 
     // Crear pipes
-    if (pipe(p1) == -1 || pipe(p2) == -1)
-    {
+    if (pipe(PadreAHijo) == -1 || pipe(HijoAPadre) == -1) {
         perror("pipe");
         exit(EXIT_FAILURE);
     }
 
     // Fork
     pid_t pid = fork();
-
-    if (pid == -1)
-    {
+    if (pid == -1) {
         perror("fork");
         exit(EXIT_FAILURE);
     }
 
-    if (pid == 0)
-    { // Proceso hijo
-        // Cerrar los extremos no utilizados de los pipes
-        close(p1[1]);
-        close(p2[0]);
+    if (pid == 0) { // Hijo
+        close(PadreAHijo[1]);
+        close(HijoAPadre[0]);
 
-        while (1)
-        {
+        while (1) {
             uint8_t valor;
-            // Leer desde p1
-            if (read(p1[0], &valor, sizeof(uint8_t)) == -1)
-            {
+            
+            // Leer desde PadreAHijo
+            if (read(PadreAHijo[0], &valor, sizeof(uint8_t)) == -1) {
                 perror("read");
                 exit(EXIT_FAILURE);
             }
-            if (valor == 0)
-            {
+
+            printf("Hijo (pid=%d): %d\n", getpid(), valor);
+            if (valor == 0) {
                 break;
             }
             valor--;
-            // Escribir en p2
-            if (write(p2[1], &valor, sizeof(uint8_t)) == -1)
-            {
+
+            // Escribir en HijoAPadre
+            if (write(HijoAPadre[1], &valor, sizeof(uint8_t)) == -1) {
                 perror("write");
                 exit(EXIT_FAILURE);
             }
         }
 
-        // Cerrar los extremos de los pipes
-        close(p1[0]);
-        close(p2[1]);
+        close(PadreAHijo[0]);
+        close(HijoAPadre[1]);
         exit(EXIT_SUCCESS);
-    }
-    else
-    { // Proceso padre
-        // Cerrar los extremos no utilizados de los pipes
-        close(p1[0]);
-        close(p2[1]);
+    } else { // Padre
+        close(PadreAHijo[0]);
+        close(HijoAPadre[1]);
 
-        while (1)
-        {
-            // Escribir el número en p1
-            if (write(p1[1], &numero, sizeof(uint8_t)) == -1)
-            {
+         printf("Padre (pid=%d): Comenzare a contar desde %d hasta 0\n", getpid(), numero); // Imprimir el valor recibido por el padre
+
+        while (numero > 0) {
+            printf("Padre (pid=%d): %d\n", getpid(), numero); // Imprimir el valor recibido por el padre
+
+            numero--;
+            
+            // Escribir en PadreAHijo
+            if (write(PadreAHijo[1], &numero, sizeof(uint8_t)) == -1) {
                 perror("write");
                 exit(EXIT_FAILURE);
             }
 
-            // Leer desde p2
-            if (read(p2[0], &numero, sizeof(uint8_t)) == -1)
-            {
+            // Leer desde HijoAPadre
+            if (read(HijoAPadre[0], &numero, sizeof(uint8_t)) == -1) {
                 perror("read");
                 exit(EXIT_FAILURE);
             }
-            if (numero == 0)
-            {
-                break;
-            }
-            printf("%d\n", numero);
         }
 
-        // Cerrar los extremos de los pipes
-        close(p1[1]);
-        close(p2[0]);
+        // Cerrar pipes
+        close(PadreAHijo[1]);
+        close(HijoAPadre[0]);
 
-        // Esperar a que el proceso hijo termine
+        // Esperar a que hijo termine
         waitpid(pid, NULL, 0);
     }
 
