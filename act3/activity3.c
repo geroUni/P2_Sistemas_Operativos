51ff771a7ddbb9114abfda63b33de98609980b25
MBouzasG <131443918+MBouzasG@users.noreply.github.com>
2024-02-09 19:06:39 +0100

    Update activity3.c

--- act3/activity3.c
+++ act3/activity3.c
@@ -9,14 +9,20 @@
 #include <sys/wait.h>
 
 #define SEMF_NOMBRE "/mi_semaforo"
+#define SEMF2_NOMBRE "/mi_semaforo2"
 
 int main(int argc, char *argv[])
 {   
+    int valor_semaforo;
     key_t clave_shm; // Declaración de la clave para la memoria compartida
     int id_shm;      // Identificador de la memoria compartida
     void *ptr_shm;   // Puntero a la memoria compartida
     sem_t *sem_punt; // Puntero al semáforo
+    sem_t *sem_punt2; // Puntero al semáforo
     pid_t pid;       // Identificador de proceso (PID)
+
+    sem_unlink(SEMF_NOMBRE);
+    sem_unlink(SEMF2_NOMBRE);
 
     if (argc != 2)
     {
@@ -49,15 +55,20 @@ int main(int argc, char *argv[])
         perror("shmat");
         return 1;
     }
-    sem_unlink(SEMF_NOMBRE);
+    
     sem_punt = sem_open(SEMF_NOMBRE, O_CREAT | O_EXCL, 0644, 1);
+    sem_punt2 = sem_open(SEMF2_NOMBRE, O_CREAT | O_EXCL, 0644, 0);
 
     if (sem_punt == SEM_FAILED)
     {
         perror("sem_open");
         return 1;
     }
-    
+    if (sem_punt2 == SEM_FAILED)
+    {
+        perror("sem_open");
+        return 1;
+    }
     pid = fork();
     if (pid < 0)
     {
@@ -67,29 +78,46 @@ int main(int argc, char *argv[])
 
     int *num_compartido = (int *)ptr_shm;
     *num_compartido = num_entrada;
-
+    int value;
     if (pid == 0)
     {
-
-        while (*num_compartido >= 0)
+        while (1)
         {
-            sem_wait(sem_punt);
-            printf("Hijo recibió: %d\n", *num_compartido);
-            (*num_compartido)--;
+            sem_getvalue(sem_punt2, &value);
+            printf("Valor del semáforo antes de sem_wait(): %d\n", valor_semaforo);
+            sem_wait(sem_punt2);
+            if (*num_compartido > 0)
+            {
+                printf("Hijo recibió: %d\n", *num_compartido);
+                (*num_compartido)--;
+            }
+            else
+            {
+                sem_post(sem_punt);
+                break;
+            }
             sem_post(sem_punt);
-            sleep(0.0001);
+            sleep(0.0000001);
         }
     }
     else
     {
-        printf("Padre (pid=%d): Comenzare a contar desde %d hasta 0\n", getpid(), num_entrada); // Imprime el valor recibido por el padre
-        while (*num_compartido >= 0)
+        printf("Padre (pid=%d): Comenzare a contar desde %d hasta 0\n", getpid(), num_entrada);
+        while (1)
         {
             sem_wait(sem_punt);
-            printf("Padre envió: %d\n", *num_compartido);
-            (*num_compartido)--;
-            sem_post(sem_punt);
-            sleep(0.0001);
+            if (*num_compartido > 0)
+            {
+                printf("Padre envió: %d\n", *num_compartido);
+                (*num_compartido)--;
+            }
+            else
+            {
+                sem_post(sem_punt2);
+                break;
+            }
+            sem_post(sem_punt2);
+            sleep(0.0000001);
         }
         waitpid(pid, NULL, 0);
 
